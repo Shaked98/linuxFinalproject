418120deef09f551d36cb03a68d973c8242502da
RihanKhutaba <100447571+RihanKhutaba@users.noreply.github.com>
2022-03-14 23:42:49 +0200

    create function.sh with functions names
    deailed with input params

---
+++ function.sh
@@ -0,0 +1,64 @@
+#! /bin/bash
+
+Insert_record_function ()
+{
+# as input:record_name
+# as input:record_amount
+
+
+
+
+}
+
+Delete_record_function ()
+{
+# as input:record_name
+# as input:record_amount
+
+}
+
+Search_string_in_file ()
+{
+# as input:str
+
+
+}
+
+Update_record_name_function ()
+{
+# as input:record_name
+# as input:new_record_name
+
+
+
+
+}
+
+Update_record_amount_function ()
+{
+# as input:record_name
+# as input:record_amount
+
+
+
+}
+
+Print_total_amount_function ()
+{
+# no input parameters
+
+}
+
+Print_sorted_record_file_function ()
+{
+# no input parameters
+
+}
+
+Write_to_record_log_function ()
+{
+#input: ff_log_operaion_name (insert,delete,search,updateName,...)
+#input: f_log_status if passed or failed
+
+
+}
