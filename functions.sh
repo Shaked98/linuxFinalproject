0592c45de7faed229d68990a0973189a08a638d8
tzafrirhn <tzafrirhn@gmail.com>
2022-03-15 14:13:54 +0200

    Update #1.1
    Add functions insert string and amount validation

--- functions.sh
+++ functions.sh
@@ -4,7 +4,43 @@ Insert_record_function ()
 {
 # as input:record_name
 # as input:record_amount
+        # check valid string
+        read -p "Insert Record Name: " rcd
+        record_name_vld_function  $rcd
+        while [ ! $rcdchk ]
+        do
+                read -p "Invalid Record Name. Insert Record Name:" rcd
+                record_name_vld_function $rcd
+        done
+        # check valid number
+        read -p "Insert Amount of Copies: " amount
+        amount_vld_function $amount
+        while [ ! $numchk ]
+        do
+                read -p "Insert Amount of Copies: " amount
+                amount_vld_function $amount
+        done
 
+        Search_string_in_file $rcd
+        if [ -z $record_name ]
+        then
+                echo "$rcd,$amount" >> $FILE
+                #log new record
+                echo "log new record $rcd,$amount"
+        else
+                PS3="Select existing record or N - new record: "
+                select var in $record_name
+                do
+                        case $REPLY in
+                        N|n|new) echo "$rcd,$amount" >> $FILE
+                        #log new record
+                        echo "log new record $rcd,$amount"; break
+                        ;;
+                        *) Update_record_amount_function; break
+                        ;;
+                        esac
+                done
+        fi
 
 
 
@@ -65,13 +101,26 @@ Write_to_record_log_function ()
 record_name_vld_function()
 { 
 #input:expression 
-
-
+        REGEX="^[[:alnum:][:space:]]*$"
+        if [[ $1 =~ $REGEX ]]
+        then
+                rcdchk=true
+        else
+                echo "invalid input"
+                rcdchk=false
+        fi
 }
 
 amount_vld_function()
 { 
 #input: expression
+        REGEX='^[[:digit:]]+$'
+        if [[ $1 =~ $REGEX ]]
+        then
+                numchk=true
+        else
+                numchk=false
+        fi
 
 
 }
